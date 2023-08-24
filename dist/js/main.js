50b88c8c9698aee5ce3f414702ba5d331d49db1f
newRidho <newridho1298@gmail.com>
2023-08-24 14:59:44 +0700

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,25 @@
+// Navbar Fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const toTop = document.querySelector('#to-top');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    } else {
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+};
+
+//Hamburger Menu
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      screens: {
+        '2xl': '1320px',
+      },
+      colors: {},
+    },
+  },
+  plugins: [],
+}
+
